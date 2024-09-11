32ee51ec2fd0950a7008b978f95afcecfaa62ac0
Simi Ibraheem <simisolaibraheem@gmail.com>
2024-09-11 09:38:15 +0000

    margin calculation per product

--- models/intermediate/int_sales_margin.sql
+++ models/intermediate/int_sales_margin.sql
@@ -1,6 +1,12 @@
 SELECT 
-    *
-
-FROM {{ ref('stg_raw__sales') }}
-JOIN {{ ref('stg_raw__products') }}
-ON stg_raw__products.products_id = stg_raw__sales.products_id
+    products_id,
+    date_date,
+    orders_id,
+    revenue,
+    quantity,
+    purchase_price,
+    ROUND(s.quantity*p.purchase_price,2) AS purchase_cost,
+    ROUND((s.revenue - s.quantity*p.purchase_price),2) AS margin
+FROM {{ ref('stg_raw__sales') }} s
+LEFT JOIN {{ ref('stg_raw__products') }} p
+    USING (products_id)
